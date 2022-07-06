50d66c57292e14e5fb052fa54692704d39947fdb
NadaMohamedFathy <64032834+NadaMohamedFathy@users.noreply.github.com>
2022-07-06 22:30:11 +0200

    Fuzzy System

---
+++ FuzzySystem/src/ProjectFunding.java
@@ -0,0 +1,136 @@
+import java.util.ArrayList;
+
+public class ProjectFunding 
+{
+	String name = "projectFunding";
+	String shape = "trapezoidal";
+	ArrayList <Integer> point1 = new ArrayList <Integer>();
+	ArrayList <Integer> point2 = new ArrayList <Integer>();
+	public int projectFund;
+	public ProjectFunding(int projectFund)
+	{
+		this.projectFund=projectFund;
+	}
+	
+	public double veryLow()
+	{
+		ArrayList <Integer> arr_x = new ArrayList <Integer>();
+		ArrayList <Integer> arr_y = new ArrayList <Integer>();
+		arr_x.add(0);
+		arr_x.add(0);
+		arr_x.add(10);
+		arr_x.add(30);
+		arr_y.add(0);
+		arr_y.add(1);
+		arr_y.add(1);
+		arr_y.add(0);
+		point1.clear();
+		point2.clear();
+		double equation = projectFundingCalculation(arr_x,arr_y);
+		return equation;
+	}
+	
+	public double low()
+	{
+		ArrayList <Integer> arr_x = new ArrayList <Integer>();
+		ArrayList <Integer> arr_y = new ArrayList <Integer>();
+		arr_x.add(10);
+		arr_x.add(30);
+		arr_x.add(40);
+		arr_x.add(60);
+		arr_y.add(0);
+		arr_y.add(1);
+		arr_y.add(1);
+		arr_y.add(0);
+		point1.clear();
+		point2.clear();
+		double equation = projectFundingCalculation(arr_x,arr_y);
+		return equation;
+	}
+	
+	public double medium()
+	{
+		ArrayList <Integer> arr_x = new ArrayList <Integer>();
+		ArrayList <Integer> arr_y = new ArrayList <Integer>();
+		arr_x.add(40);
+		arr_x.add(60);
+		arr_x.add(70);
+		arr_x.add(90);
+		arr_y.add(0);
+		arr_y.add(1);
+		arr_y.add(1);
+		arr_y.add(0);
+		point1.clear();
+		point2.clear();
+		double equation = projectFundingCalculation(arr_x,arr_y);
+		return equation;
+	}
+	
+	public double high()
+	{
+		ArrayList <Integer> arr_x = new ArrayList <Integer>();
+		ArrayList <Integer> arr_y = new ArrayList <Integer>();
+		arr_x.add(70);
+		arr_x.add(90);
+		arr_x.add(100);
+		arr_x.add(100);
+		arr_y.add(0);
+		arr_y.add(1);
+		arr_y.add(1);
+		arr_y.add(0);
+		point1.clear();
+		point2.clear();
+		double equation = projectFundingCalculation(arr_x,arr_y);
+		return equation;
+	}
+	public double projectFundingCalculation(ArrayList <Integer> arr_x, ArrayList <Integer> arr_y)
+	{
+		for(int i=0; i<arr_x.size(); i++)
+		{
+			if (projectFund<arr_x.get(0) || (i+1)>=arr_x.size())
+			{
+				point1.add(0);
+				point1.add(0);
+				point2.add(0);
+				point2.add(0);
+				return 0;
+			}
+			else if ((i+1)<arr_x.size() && projectFund>arr_x.get(i) && projectFund<arr_x.get(i+1))
+			{
+				point1.add(arr_x.get(i));
+				point1.add(arr_y.get(i));
+				point2.add(arr_x.get(i+1));
+				point2.add(arr_y.get(i+1));
+				break;
+			}
+			else if (projectFund==arr_x.get(i)) //netaked 
+			{
+				point1.add(arr_x.get(i));
+				point1.add(arr_y.get(i));
+				if ((i+1)<arr_x.size())
+				{
+					point2.add(arr_x.get(i+1));
+					point2.add(arr_y.get(i+1));
+					break;
+				}
+				else
+				{
+					point2.add(arr_x.get(i-1));
+					point2.add(arr_y.get(i-1));
+					break;
+				}
+			}	
+		}
+		double equation = equationCalculation();
+		return equation;
+	}
+	public double equationCalculation()
+	{
+		double y = point2.get(1)-point1.get(1);
+		double x = point2.get(0)-point1.get(0);
+		double slope = y/x;
+		double b = point1.get(1) - (slope*point1.get(0));
+		double equation = (slope * projectFund) + b; 
+		return equation;
+	}
+}
